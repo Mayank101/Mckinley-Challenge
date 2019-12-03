3b2f59e879e2ee8bd8c323e3202f0dc34d36ff63
Mayank Kakkar <mayank@Mayanks-MacBook-Air.local>
2019-12-04 00:03:00 +0530

    Second commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
